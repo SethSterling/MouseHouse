c0adc9cea69e715af72649c677685b7a25fca8cf
Spencer Sterling <63821532+SpencerTSterling@users.noreply.github.com>
2021-01-23 20:36:53 -0800

    Customized ASP.NET Identity pages (#15)
    * Scaffolded all ASP.NET Identity pages
    
    * Renamed database to MouseHouseDb
    
    * Created method SetIdentityOptions to hold Identity options
    
    * Created IdentityHelper class to hold methods related to Identity
    
    * Modified Register page to allow username/email seperate + removed external registration
    
    * Modified Login page for use of usernames + removed external log in

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using MouseHouse.Data;
+using MouseHouse.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,10 +31,35 @@ namespace MouseHouse
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<IdentityUser>(IdentityHelper.SetIdentityOptions)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
+        }
+
+        /// <summary>
+        /// Sets Identity options for sign in procedures, passwords, and lockout options:
+        /// Sign in options don't require a confirmed email or phone number.
+        /// Password strength requirement is only 8 characters, no required uppercase/lowercase/digits/special characters.
+        /// Lock out timespan is 10 minutes with maximum 5 failed attempts. 
+        /// </summary>
+        /// <param name="options"></param>
+        public static void SetIdentityOptions(IdentityOptions options)
+        {
+            // sign in options
+            options.SignIn.RequireConfirmedEmail = false;
+            options.SignIn.RequireConfirmedPhoneNumber = false;
+
+            // password options
+            options.Password.RequireDigit = false;
+            options.Password.RequireLowercase = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequiredLength = 8;
+
+            // lockout options
+            options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(10);
+            options.Lockout.MaxFailedAccessAttempts = 5;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
