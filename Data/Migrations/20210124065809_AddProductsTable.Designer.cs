734913d1f4c07caf9d93144a897b10b44bc89ba9
Spencer Sterling <spencer.sterling2001@gmail.com>
2021-01-23 22:58:51 -0800

    Add migration for Products table

--- Data/Migrations/20210124065809_AddProductsTable.Designer.cs
+++ Data/Migrations/20210124065809_AddProductsTable.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MouseHouse.Data;
 
 namespace MouseHouse.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210124065809_AddProductsTable")]
+    partial class AddProductsTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
