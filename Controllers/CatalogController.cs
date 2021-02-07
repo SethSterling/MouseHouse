288180953ea5b2d53de0460c8631ebd023ecd8bc
Spencer Sterling <63821532+SpencerTSterling@users.noreply.github.com>
2021-02-06 20:41:54 -0800

    Customers can now view the catalog (#26)
    * Created CatalogController for customers to see the product listing
    
    * Removed Create, Edit, and Delete links from the Catalog Index view
    
    * Removed Create, Edit, Delete Actions from the CatalogController
    
    * Deleted the view files for Create, Edit, and Delete from Catalog folder

---
+++ Controllers/CatalogController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MouseHouse.Data;
+using MouseHouse.Models;
+
+namespace MouseHouse.Controllers
+{
+    public class CatalogController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CatalogController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Catalog
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Products.ToListAsync());
+        }
+
+        // GET: Catalog/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        private bool ProductExists(int id)
+        {
+            return _context.Products.Any(e => e.ProductId == id);
+        }
+    }
+}
