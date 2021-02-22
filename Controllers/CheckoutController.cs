1c5fe1701e3c6209cc39b207b5628c8f5566c50f
Spencer Sterling <63821532+SpencerTSterling@users.noreply.github.com>
2021-02-22 00:00:35 -0800

    Proceed to Checkout (#32)
    * Created Order class
    
    * Created the CheckoutController
    
    * AddressAndPayment view form in checkout process
    
    * Added DbSet for Orders
    
    * Added Proceed to Checkout button and showed cart's total on Cart summary page
    
    * Added Complete view for CheckoutController
    
    * Refactored CheckoutController

---
+++ Controllers/CheckoutController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MouseHouse.Data;
+using MouseHouse.Models;
+
+namespace MouseHouse.Controllers
+{
+    [Authorize]
+    public class CheckoutController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IHttpContextAccessor _httpContext;
+        public CheckoutController(ApplicationDbContext context, IHttpContextAccessor httpContext)
+        {
+            _context = context;
+            _httpContext = httpContext;
+        }
+
+        // GET: Checkout/AddressAndPayment
+        public IActionResult AddressAndPayment()
+        {
+            return View();
+        }
+
+        // POST: Checkout/AddressAndPayment
+        [HttpPost]
+        public IActionResult AddressAndPayment(IFormCollection form)
+        {
+            var order = new Order();
+            TryUpdateModelAsync(order);
+
+            order.Username = User.Identity.Name;
+            order.OrderDate = DateTime.Now;
+
+            _context.Orders.Add(order);
+            _context.SaveChanges();
+
+            return Redirect("Complete");
+        }
+
+        // GET: Checkout/Complete
+        public IActionResult Complete()
+        {
+            return View();
+        }
+    }
+}
---
+++ Data/Migrations/20210222073314_OrderNumberKey.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MouseHouse.Data.Migrations
+{
+    public partial class OrderNumberKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OrderNumber",
+                table: "Products",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    OrderNumber = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(nullable: true),
+                    FirstName = table.Column<string>(maxLength: 160, nullable: true),
+                    LastName = table.Column<string>(maxLength: 160, nullable: true),
+                    Address = table.Column<string>(maxLength: 70, nullable: true),
+                    City = table.Column<string>(maxLength: 70, nullable: true),
+                    State = table.Column<string>(maxLength: 70, nullable: true),
+                    ZIPCode = table.Column<string>(maxLength: 70, nullable: true),
+                    Country = table.Column<string>(maxLength: 70, nullable: true),
+                    Phone = table.Column<string>(maxLength: 70, nullable: true),
+                    Email = table.Column<string>(maxLength: 160, nullable: true),
+                    Total = table.Column<decimal>(nullable: false),
+                    OrderDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.OrderNumber);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_OrderNumber",
+                table: "Products",
+                column: "OrderNumber");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Orders_OrderNumber",
+                table: "Products",
+                column: "OrderNumber",
+                principalTable: "Orders",
+                principalColumn: "OrderNumber",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Orders_OrderNumber",
+                table: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_OrderNumber",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "OrderNumber",
+                table: "Products");
+        }
+    }
+}
