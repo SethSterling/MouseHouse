6d9f914551e762a976e30d16a0d6782716127bfa
SethSterling <63669378+SethSterling@users.noreply.github.com>
2021-02-21 20:29:49 -0800

    Issue#11 shopping cart (#30)
    * Added Cart Controller
    
    * Added Cart helper method
    
    * Added list page for cart products
    
    * Added put in cart option in catalog
    
    * Added link to view cart in main nav

---
+++ Controllers/CartController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MouseHouse.Data;
+using MouseHouse.Models;
+
+namespace MouseHouse.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IHttpContextAccessor _httpContext;
+        public CartController(ApplicationDbContext context, IHttpContextAccessor httpContext)
+        {
+            _context = context;
+            _httpContext = httpContext;
+        }
+
+        public async Task<IActionResult> Add(int id, string prevUrl)
+        {
+            // Get product from the database
+            Product p = await _context.Products.FirstOrDefaultAsync(m => m.ProductId == id);
+
+            List<Product> cartProducts = CartCookieHelper.GetCartProducts(_httpContext);
+            CartCookieHelper.AddProductToCart(_httpContext, p);
+
+            TempData["Message"] = $"{p.Title} was added to your cart";
+
+            // Redirect back to previous page
+            return Redirect(prevUrl);
+        }
+
+        public async Task<IActionResult> Delete(int id, string prevUrl)
+        {
+            // get product from database
+            Product p = await _context.Products.FirstOrDefaultAsync(m => m.ProductId == id);
+
+            List<Product> cartProducts = CartCookieHelper.GetCartProducts(_httpContext);
+            CartCookieHelper.RemoveProductFromCart(_httpContext, id);
+
+            List<Product> updatedCartProducts = CartCookieHelper.GetCartProducts(_httpContext);
+
+            // redirect back to previous page
+            return Redirect(prevUrl);
+        }
+
+        public IActionResult Summary()
+        {
+            List<Product> cartProducts =
+                CartCookieHelper.GetCartProducts(_httpContext);
+
+            return View(cartProducts);
+        }
+
+        public IActionResult ClearCart(string prevUrl)
+        {
+            // clear all items from the cart (delete the cookie)
+            CartCookieHelper.EmptyCart(_httpContext);
+
+            return Redirect(prevUrl);
+        }
+    }
+}
