c501ddb80096c509a8b53ea9ef88fd6c1540007e
Spencer Sterling <spencer.sterling2001@gmail.com>
2021-02-21 23:01:40 -0800

    Created the CheckoutController

---
+++ Controllers/CheckoutController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MouseHouse.Controllers
+{
+    [Authorize]
+    public class CheckoutController : Controller
+    {
+        // GET: Checkout/AddressAndPayment
+        public IActionResult AddressAndPayment()
+        {
+            return View();
+        }
+
+        // POST: Checkout/AddressAndPayment
+        [HttpPost]
+        public IActionResult AddressAndPayment(IFormCollection form)
+        {
+            return View();
+        }
+
+        // GET: Checkout/Complete
+        public IActionResult Complete()
+        {
+            return View();
+        }
+    }
+}
