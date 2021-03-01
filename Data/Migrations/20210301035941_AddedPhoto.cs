d1b479cb418e228c54988a43b7e3c8c2f6c9895d
Spencer Sterling <spencer.sterling2001@gmail.com>
2021-02-28 20:05:24 -0800

    Added migration and updated database for Image property

---
+++ Data/Migrations/20210301035941_AddedPhoto.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MouseHouse.Data.Migrations
+{
+    public partial class AddedPhoto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
