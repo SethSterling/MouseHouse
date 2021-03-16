ab77d58a3ae568daf1223478690e353086a6f8f4
SethSterling <63669378+SethSterling@users.noreply.github.com>
2021-03-15 21:31:12 -0700

    Issue#68 orders controller (#72)
    * Added Orders Controller with views
    
    * Added link in nav

---
+++ Controllers/OrdersController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MouseHouse.Data;
+using MouseHouse.Models;
+
+
+namespace MouseHouse.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrdersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Orders
+        public async Task<IActionResult> Index()
+        {
+            var orders = from o in _context.Orders
+                         where o.Username == User.Identity.Name
+                         select o;
+
+            return View( await orders.AsNoTracking().ToListAsync());
+        }
+
+        // GET: Orders/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Orders
+                .FirstOrDefaultAsync(m => m.OrderNumber == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return View(order);
+        }
+
+        private bool OrderExists(int id)
+        {
+            return _context.Orders.Any(e => e.OrderNumber == id);
+        }
+    }
+}
